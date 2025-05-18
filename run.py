f7179f3a7eafcfcf012dd1121ff43bddf0139f08
Robert Veloya <80503404+jrveloya@users.noreply.github.com>
2025-05-18 15:48:45 -0700

    added debug endpoints
    added a health check and a

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app import config
 
@@ -6,6 +7,10 @@ app = create_app()
 @app.route('/health', methods=['GET'])
 def health_check():
     return 'OK', 200
+
+@app.route("/debug/db")
+def debug_db():
+    return os.environ.get("MONGO_URI", "Not set")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5050, host='0.0.0.0')
